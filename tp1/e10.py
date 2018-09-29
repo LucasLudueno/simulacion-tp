d21b9f89c5bb3f222dc09d422560af44e185b793
Lucas Ludueno <ludueno.lucas@mulesoft.com>
2018-09-29 19:10:47 -0300

    10 and 8

--- tp1/e10.py
+++ tp1/e10.py
@@ -1,32 +1,62 @@
-import scipy.stats as stats
-
 import math
 import random
 import numpy
+import scipy.stats as stats
 import matplotlib.pyplot as plt
 from statistics import median, variance, mode, StatisticsError
 from gcl import GCL
 
-CANTIDAD_MUESTRAS = 100
+CANTIDAD_MUESTRAS = 100000
 NIVEL_SIGNIFICACION = 0.01
 
 def p(x):
-  media = 1
-  desvio = 1
-  c = math.sqrt(2 * math.exp(media) / math.pi)
-  return normal(x, media, desvio) / (c * exp(x, media))
+  c = math.sqrt(2 * math.exp(1) / math.pi)
+  return normal(x) / (c * exp(x))
 
-def exp(x, media = 1):
-  return math.exp(x * media)
+def exp(x): # exponencial negativa media 1
+  return math.exp(x * -1)
 
-def normal(x, media = 1, desvio = 1):
-  return math.exp(- (x - media)**2 / 2*(desvio**2) ) / ( math.sqrt(2 * math.pi) * desvio )
+def normal(x): # normal estandar
+  return math.exp(-1 * (x**2) / 2) / math.sqrt(2 * math.pi)
+
+def calculate_intervals(media, cant):
+  interval_list = []
+  lenght = int(cant / 2)
+
+  for i in reversed(range(lenght - 1)): # puntos negativos
+    begin = media/lenght * (i + 1) * -1
+    end = media/lenght * i * -1
+    interval_list.append((begin, end))
+
+  for i in range(0, lenght): # puntos positivos
+    begin = media/lenght * i
+    end = media/lenght * (i + 1)
+    interval_list.append((begin, end))
+  
+  return interval_list
+
+def empiric_fuc(distr_list, intervals):
+  empiric_distr = []
+
+  for i in range(len(intervals)):
+    empiric_distr.append(0)
+    interval = intervals[i]
+
+    for x in distr_list:
+      if interval[0] <= x and x < interval[1]:
+        empiric_distr[i] += 1
+    
+    if i != 0:
+      empiric_distr[i] += empiric_distr[i - 1]
+  
+  return empiric_distr
+    
 
 # Paso 1: Generamos 100000 muestras de una variable exponencial
 exponential_samples = [numpy.random.exponential(1) for x in range(CANTIDAD_MUESTRAS)]
 
 # Paso 2: Aplicamos cada una de las muestras a nuestra funcion de probabilidad y en base a ello las elegimos
-normal_distrib_list = []
+normal_list = []
 
 for i in range(CANTIDAD_MUESTRAS):
   sample = exponential_samples[i]
@@ -36,20 +66,35 @@ for i in range(CANTIDAD_MUESTRAS):
     r2 = random.random()
 
     if (r2 < 0.5): # con prob 0.5 lo dejo positivo
-      normal_distrib_list.append(sample)
+      normal_list.append(sample)
     else:
-      normal_distrib_list.append(sample * -1)
+      normal_list.append(sample * -1)
+
 
 # Paso 3: Teniendo los valores de la normal, ejecutamos el Test de Kolmogorov
-# normed_data = (normal_distrib_list - mu) / sigma # Normalizamos a 0 - 1
-D, pval = stats.kstest(normal_distrib_list, 'norm')
+# para una normal estandar (no hace falta aplicar la transformacion lineal)
+D, pval = stats.kstest(normal_list, 'norm')
 
 if pval < NIVEL_SIGNIFICACION:
   print('Rechazamos la hipotesis')
 else:
   print('Aceptamos la hipotesis')
 
+# Calculo la funcion empirica de la distribucion generada
+intervals = calculate_intervals(3, 10) #calculo para 10 intervalos y media 3
+points_x = [x[1] for x in intervals]
+empiric_points_y = empiric_fuc(normal_list, intervals)
 
-# https://gist.github.com/devries/11405101
+# Calculo la funcion de acumulacion para la funcion real
+normal_point = numpy.random.normal(0, 1, len(normal_list))
+empiric_real_points_y = empiric_fuc(normal_point, intervals)
 
-# https://gist.github.com/jfbercher/3601a9f2595d49e35475
+# Normalizamos ambos vectores
+max_value_to_normalize_generated = max(empiric_points_y)
+max_value_to_normalize_real = max(empiric_real_points_y)
+
+# Graficamos
+plt.plot(points_x, [x / max_value_to_normalize_generated for x in empiric_points_y ], label='Distr acumulacion generada')
+plt.plot(points_x, [x / max_value_to_normalize_generated for x in empiric_real_points_y ], label='Distr acumulacion real')
+plt.legend()
+plt.show()
--- tp1/e8.py
+++ tp1/e8.py
@@ -1,31 +1,36 @@
 import statistics 
 import random
+import numpy
 import scipy.stats as stats
 import matplotlib.pyplot as plt
 
-CANTIDAD_MUESTRAS = 1000
+CANTIDAD_MUESTRAS = 10
 NIVEL_SIGNIFICACION = 0.01
 
 # Cantidad de pasos para que de cara una moneda
 def stepsForHead():
-    count=0
+    count = 0
     num = random.uniform(0,1)
-    while(num<=0.5):
-        count+=1
+
+    while(num <= 0.5):
+        count += 1
         num = random.uniform(0,1)
     return count    
 
 # Paso 1. Calculamos la cantidad de caras que se necesitan para 100.000 intentos
 stepsCount = list()
-for x in range(0, CANTIDAD_MUESTRAS):
+for x in range(1, CANTIDAD_MUESTRAS):
     stepsCount.append(stepsForHead())
 
-# Paso 2: Ejecutamos el Test de Chi 2 para los valores obtenidos
-D, pval = stats.chisquare(stepsCount)
+# Paso 2: Ejecutamos el Test de Chi 2 para los valores obtenidos (comparado con una bernoulli)
+expected_distrib = numpy.random.binomial(1, 0.5, CANTIDAD_MUESTRAS) # Bernoulli 0.5
+print(expected_distrib)
+print(stepsCount)
+D, pval = stats.chisquare(stepsCount, f_exp = expected_distrib)
+print(pval)
 
 # Paso 3: Analizamos si se cumple nuestra hipotesis
 if pval < NIVEL_SIGNIFICACION:
   print('Rechazamos la hipotesis')
 else:
   print('Aceptamos la hipotesis')
-
