5e1e1a3f30f407ad1ccf6dc26adc0aab2c83065a
Lucas Ludueno <ludueno.lucas@mulesoft.com>
2018-10-02 16:17:38 -0300

    Fix 8

--- tp1/e8.py
+++ tp1/e8.py
@@ -1,36 +1,47 @@
-import statistics 
+import statistics
 import random
 import numpy
 import scipy.stats as stats
 import matplotlib.pyplot as plt
 
-CANTIDAD_MUESTRAS = 10
+CANTIDAD_MUESTRAS = 100000
 NIVEL_SIGNIFICACION = 0.01
 
 # Cantidad de pasos para que de cara una moneda
 def stepsForHead():
-    count = 0
-    num = random.uniform(0,1)
+   count=0
+   num = random.uniform(0,1)
+   while(num<=0.5):
+       count+=1
+       num = random.uniform(0,1)
+   return count   
 
-    while(num <= 0.5):
-        count += 1
-        num = random.uniform(0,1)
-    return count    
-
-# Paso 1. Calculamos la cantidad de caras que se necesitan para 100.000 intentos
+# Paso 1. Calculamos la cantidad de caras que se necesitan para 10.000 intentos
 stepsCount = list()
-for x in range(1, CANTIDAD_MUESTRAS):
-    stepsCount.append(stepsForHead())
+for x in range(0, CANTIDAD_MUESTRAS):
+   stepsCount.append(stepsForHead())
 
-# Paso 2: Ejecutamos el Test de Chi 2 para los valores obtenidos (comparado con una bernoulli)
-expected_distrib = numpy.random.binomial(1, 0.5, CANTIDAD_MUESTRAS) # Bernoulli 0.5
-print(expected_distrib)
-print(stepsCount)
-D, pval = stats.chisquare(stepsCount, f_exp = expected_distrib)
-print(pval)
+# Paso 2. Calculamos la distribucion acumulada
+steps = {}
+for count in stepsCount:
+    if count in steps:
+        steps[count] += 1
+    else:
+        steps[count] = 1
 
-# Paso 3: Analizamos si se cumple nuestra hipotesis
+stepList = []
+for key in steps:
+    stepList.append(steps[key])
+
+sortedList = sorted(stepList, reverse=True)
+sumatory = sum(sortedList)
+normalized_distr = [x/sumatory for x in sortedList]
+
+# Paso 3: Ejecutamos el Test de Chi 2 para los valores obtenidos
+D, pval = stats.chisquare(normalized_distr)
+
+# Paso 4: Analizamos si se cumple nuestra hipotesis
 if pval < NIVEL_SIGNIFICACION:
-  print('Rechazamos la hipotesis')
+ print('Rechazamos la hipotesis')
 else:
-  print('Aceptamos la hipotesis')
+ print('Aceptamos la hipotesis')
