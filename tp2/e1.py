d6d1c6e45c0a73cba030c1e8349311ff222dc291
LucasLudueno <ludueno.lucas@gmail.com>
2018-11-13 00:55:50 -0300

    update

--- tp2/e1.py
+++ tp2/e1.py
@@ -11,6 +11,8 @@ wating_time_centralized = 0
 wating_time_distributed = 0
 no_processed_centralized = 0
 no_processed_distributed = 0
+total_wating_time_centralized = 0
+total_wating_time_distributed = 0
 
 # Simulamos las solicitudes para la opcion 1 (DB centralizada)
 current_time = 0
@@ -27,10 +29,11 @@ for x in range(0, CANTIDAD_MUESTRAS):
         wating_time = current_time - time_to_arrive
         wating_time_centralized = wating_time_centralized + wating_time
         current_time = wating_time + time_to_process
+    total_wating_time_centralized = total_wating_time_centralized + current_time
 
+# Simulamos las solicitudes para la opcion 2 (DB distribuida)
 current_time_1 = 0
 current_time_2 = 0
-# Simulamos las solicitudes para la opcion 2 (DB distribuida)
 for x in range(0, CANTIDAD_MUESTRAS):
     time_to_arrive = np.random.poisson(4)
     rand = random.random()
@@ -46,6 +49,8 @@ for x in range(0, CANTIDAD_MUESTRAS):
             wating_time = current_time_1 - time_to_arrive
             wating_time_distributed = wating_time_distributed + wating_time
             current_time_1 = wating_time + time_to_process
+        total_wating_time_distributed = total_wating_time_distributed + current_time_1
+       
     else:
         current_time_1 = max(0, current_time_1 - time_to_arrive)
         time_to_process = np.random.poisson(1)
@@ -57,11 +62,16 @@ for x in range(0, CANTIDAD_MUESTRAS):
             wating_time = current_time_2 - time_to_arrive
             wating_time_distributed = wating_time_distributed + wating_time
             current_time_2 = wating_time + time_to_process
+        total_wating_time_distributed = total_wating_time_distributed + current_time_2
 
-# Calculamos el tiempo medio para las opciones
-print("Tiempo medio para opcion DB distribuida: ", float(wating_time_distributed)  / CANTIDAD_MUESTRAS)
-print("Tiempo medio para opcion DB centralizada: ", float(wating_time_centralized) / CANTIDAD_MUESTRAS)
+# Calculamos el tiempo medio de espera para las opciones
+print("Tiempo medio de espera para opcion DB distribuida: ", float(wating_time_distributed)  / CANTIDAD_MUESTRAS)
+print("Tiempo medio de espera para opcion DB centralizada: ", float(wating_time_centralized) / CANTIDAD_MUESTRAS)
 
 # Calculamos la fraccion que no tuvo que esperar para ser procesada
 print("Fraccion de solicitudes que no esperaron a ser procesada para opcion DB distribuida: ", float(no_processed_distributed) / CANTIDAD_MUESTRAS)
 print("Fraccion de solicitudes que no esperaron a ser procesada para opcion DB centralizada: ", float(no_processed_centralized) / CANTIDAD_MUESTRAS)
+
+# Calculamos el tiempo medio total para las opciones
+print("Tiempo medio total para opcion DB distribuida: ", float(total_wating_time_distributed)  / CANTIDAD_MUESTRAS)
+print("Tiempo medio total para opcion DB centralizada: ", float(total_wating_time_centralized) / CANTIDAD_MUESTRAS)
